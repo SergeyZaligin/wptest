1ed08b504277d39dc980506e2b221138e6e0e7e3
Sergey Zaligin <webdevjsphp@gmail.com>
2018-09-02 11:06:39 +0300

    fix

--- wp-content/plugins/sz-first/index.php
+++ wp-content/plugins/sz-first/index.php
@@ -7,4 +7,38 @@ Version: 1.0
 Author: SuslikEst
 Author URI: http://mysite.ru
 */
-?>
+// function sz_activate()
+// {
+//   if(version_compare(PHP_VERSION, '5.3.0', '<')){
+//     exit('Version error');
+//   }
+// }
+// register_activation_hook( __FILE__, 'sz_activate' );
+
+/**
+ * Class activated plg
+ */
+class szPluginActivation {
+
+  public function __construct()
+  {
+    register_activation_hook(__FILE__, array($this, 'isVersion'));
+    register_activation_hook(__FILE__, array($this, 'logs'));
+  }
+
+  public function isVersion()
+  {
+    if(version_compare(PHP_VERSION, '5.4.0', '<')){
+      exit('Версия должна быть не ниже 7.1.0');
+    }
+  }
+
+  public function logs()
+  {
+    $date = date("Y-m-d H:i:s");
+    error_log("[" . $date  .  "]". " Плагин активирован\r\n", 3, dirname(__FILE__) . '/tmp/logs/sz_errors_log.log');
+  }
+
+}
+
+$szPlgActivated = new szPluginActivation();
